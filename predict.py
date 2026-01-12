97a9796dd682237faaa5c6a295577a6e12e7433b
aminecharfeddine <85786567+aminecharfeddine@users.noreply.github.com>
2026-01-12 12:39:53 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,30 +1,25 @@
 import joblib
-import numpy as np
 
-MODEL_STEP1 = "models/model_stage1.pkl"
-MODEL_STEP2 = "models/model_stage2.pkl"
-SCALER_PATH = "models/scaler.pkl"
+PIPE_STEP1 = "models/model_step1_pipeline.pkl"
+PIPE_STEP2 = "models/model_step2_pipeline.pkl"
 
-model_step1 = joblib.load(MODEL_STEP1)
-model_step2 = joblib.load(MODEL_STEP2)
-scaler = joblib.load(SCALER_PATH)
+pipe_step1 = joblib.load(PIPE_STEP1)
+pipe_step2 = joblib.load(PIPE_STEP2)
 
 def predict_eye(X):
-    X_scaled = scaler.transform(X)
-
     # Étape 1 : Normal vs KC
-    p_kc = model_step1.predict_proba(X_scaled)[0][1]
+    p_kc = pipe_step1.predict_proba(X)[0][1]
 
-    if p_kc > 0.5:
+    if p_kc >= 0.5:
         return {
             "diagnosis": "Kératocône avéré",
             "probability": round(p_kc, 3)
         }
 
     # Étape 2 : Normal vs Fruste
-    p_fruste = model_step2.predict_proba(X_scaled)[0][1]
+    p_fruste = pipe_step2.predict_proba(X)[0][1]
 
-    if p_fruste > 0.5:
+    if p_fruste >= 0.6:
         return {
             "diagnosis": "Kératocône fruste (suspect)",
             "probability": round(p_fruste, 3)
